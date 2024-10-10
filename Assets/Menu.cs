ab9fabd74e17e5a2778f57bf3a32b8bae332f2bd
jaider4419 <jaiderboateng8@gmail.com>
2024-10-10 11:48:26 +0100

    aaa

---
+++ Assets/Menu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("CutScene");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit!");
+    }
+
+
+}
